b52c97b042d71f84eec656a75f51e949ac5fff52
jefersongjr <jeferson_gjr@hotmail.com>
2022-11-17 16:38:00 -0300

    ⚡️ feat: criando a Model Car

---
+++ src/Models/CarODM.ts
@@ -0,0 +1,32 @@
+import {
+    Model,
+    model,
+    models,
+    Schema,
+} from 'mongoose';
+import ICar from '../Interfaces/ICar';
+
+class CarODM {
+    private schema: Schema;
+    private model: Model<ICar>;
+
+    constructor() {
+        this.schema = new Schema<ICar>({
+            model: { type: String, required: true },
+            year: { type: String, required: true },
+            color: { type: String, required: true },
+            status: { type: Boolean, required: true },
+            buyValue: { type: Number, required: true },
+            doorsQty: { type: Number, required: true },
+            seatsQty: { type: Number, required: true },
+        });
+        
+        this.model = models.cars || model('cars', this.schema);
+    }
+
+    public async create( car: ICar): Promise<ICar> {
+        return this.model.create({ ...car })
+    }
+}
+
+export default CarODM;
