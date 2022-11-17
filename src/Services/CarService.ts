a0f2302e377582ecc5f7a1ca6854b1b45b70ba41
jefersongjr <jeferson_gjr@hotmail.com>
2022-11-17 17:26:10 -0300

    ⚡️  feat: criando a camada Service para Car

---
+++ src/Services/CarService.ts
@@ -0,0 +1,27 @@
+import Car from "../Domains/Car";
+import ICar from "../Interfaces/ICar";
+import CarODM from "../Models/CarODM";
+
+class CarService {
+    private createCarDomain(car: ICar | null): Car | undefined | null {
+        if(car) {
+            return new Car(
+                car.id,
+                car.model,
+                car.year,
+                car.color,
+                car.status,
+                car.buyValue,
+                car.doorsQty,
+                car.seatsQty, 
+            );
+        }
+        return null;
+    }
+    carODM = new CarODM();
+    public async addCar(car: ICar) {
+    const newCar = await this.carODM.create(car);
+
+    return this.createCarDomain(newCar)
+    }
+}
