7e160aea2b86942591df4bee18b44bcd2d5c9154
jefersongjr <jeferson_gjr@hotmail.com>
2022-11-18 19:19:24 -0300

    ✨ feat: criando mocks do test

--- src/utils/Mocks.ts
+++ src/utils/Mocks.ts
@@ -1,26 +1,47 @@
-import Car from "../Domains/Car";
-import ICar from "../Interfaces/ICar";
+import Car from '../Domains/Car';
+import ICar from '../Interfaces/ICar';
 
 export const addCarInput: ICar = {
-  "model": "Marea",
-  "year": 2002,
-  "color": "Black",
-  "status": true,
-  "buyValue": 15.990,
-  "doorsQty": 4,
-  "seatsQty": 5
-
-}
+  model: 'Marea',
+  year: 2002,
+  color: 'Black',
+  status: true,
+  buyValue: 15.990,
+  doorsQty: 4,
+  seatsQty: 5,
+};
 
 export const addCarOutput: Car = new Car({
-  "id": "634852326b35b59438fbea2f",
-  "model": "Marea",
-  "year": 2002,
-  "color": "Black",
-  "status": true,
-  "buyValue": 15.99,
-  "doorsQty": 4,
-  "seatsQty": 5,
-  }
-);
-  
+  id: '634852326b35b59438fbea2f',
+  model: 'Marea',
+  year: 2002,
+  color: 'Black',
+  status: true,
+  buyValue: 15.99,
+  doorsQty: 4,
+  seatsQty: 5,
+});
+
+export const allCarsArray: ICar[] = [{
+  id: '634852326b35b59438fbea2f',
+  model: 'Marea',
+  year: 2002,
+  color: 'Black',
+  status: true,
+  buyValue: 15.99,
+  doorsQty: 4,
+  seatsQty: 5,
+},
+{
+  id: '634852326b35b59438fbea31',
+  model: 'Tempra',
+  year: 1995,
+  color: 'Black',
+  status: false,
+  buyValue: 39,
+  doorsQty: 2,
+  seatsQty: 5,
+},
+];
+
+export const carOutput = allCarsArray.map((car) => new Car(car));
