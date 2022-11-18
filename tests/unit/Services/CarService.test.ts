209e959e655b0f6f45c54423b67798d950d67cff
jefersongjr <jeferson_gjr@hotmail.com>
2022-11-18 19:13:50 -0300

    ✨ feat: criando testes para a tota get "/cars"

--- tests/unit/Services/CarService.test.ts
+++ tests/unit/Services/CarService.test.ts
@@ -2,16 +2,24 @@ import { expect } from 'chai';
 import sinon from 'sinon';
 import { Model } from 'mongoose';
 import CarService from '../../../src/Services/CarService';
-import { addCarInput, addCarOutput } from '../../../src/utils/Mocks';
+import { addCarInput, addCarOutput, carOutput } from '../../../src/utils/Mocks';
 
-describe('Testa o cadastro de novos carros', () => {
-  it('Adiciona um novo carro com sucesso', async () => {
-    
+describe('Testa a Service da rota "/cars"', function () {
+  it('testa a rota post que adiciona um novo carro com sucesso', async function () {
     sinon.stub(Model, 'create').resolves(addCarOutput);
 
     const service = new CarService();
-    const result =  await service.addCar(addCarInput);
+    const result = await service.addCar(addCarInput);
 
     expect(result).to.be.deep.equal(addCarOutput);
-  })
-})
+  });
+
+  it('Testa a rota get que lista todos os carros', async function () {    
+    sinon.stub(Model, 'find').resolves(carOutput);
+
+    const service = new CarService();
+    const result = await service.getCars();
+
+    expect(result).to.be.deep.equal(carOutput);
+  });
+});
