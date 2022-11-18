ae3e04cf0a5b9cd18e2017f2c8326885d95074b4
jefersongjr <jeferson_gjr@hotmail.com>
2022-11-18 20:05:26 -0300

    ✨ feat: criando teste para o retorno Invalid mongo id

--- tests/unit/Services/CarService.test.ts
+++ tests/unit/Services/CarService.test.ts
@@ -22,4 +22,18 @@ describe('Testa a Service da rota "/cars"', function () {
 
     expect(result).to.be.deep.equal(carOutput);
   });
+  
+  it('Testa a rota get que lista um carro por ID', async function () {    
+    sinon.stub(Model, 'find').resolves(carOutput);
+    try {
+      const service = new CarService();
+      await service.getOneCarsById('634852326b35b59438fb');
+    } catch (error) {
+      expect((error as Error).message).to.be.equal('Invalid mongo id');
+    }
+  });
+  
+  afterEach(function () {
+    sinon.restore();
+  });
 });
