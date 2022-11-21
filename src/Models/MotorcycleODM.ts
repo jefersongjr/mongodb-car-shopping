0da9b2c466fb024e3ef4a7795124f3bd71731cfc
jefersongjr <jeferson_gjr@hotmail.com>
2022-11-21 18:52:42 -0300

    ✨ feat: criando odm motorcycles

--- src/Models/MotorcycleODM.ts
+++ src/Models/MotorcycleODM.ts
@@ -1,33 +1,23 @@
 import {
-    Model,
-    model,
-    models,
-    Schema,
-  } from 'mongoose';
-  import IMotorcycle from '../Interfaces/IMotorcycle';
-  import ThrowException from '../Middlewares/exceptions/ThrowException';
+  Schema,
+} from 'mongoose';
+import IMotorcycle from '../Interfaces/IMotorcycle';
+import AbstractODM from './AbstractODM';
   
-  class MotorcycleODM {
-    private schema: Schema;
-    private model: Model<IMotorcycle>;
-  
-    constructor() {
-      this.schema = new Schema<IMotorcycle>({
-        model: { type: String, required: true },
-        year: { type: Number, required: true },
-        color: { type: String, required: true },
-        status: { type: Boolean, required: false },
-        buyValue: { type: Number, required: true },
-        category: { type: Number, required: true },
-        engineCapacity: { type: Number, required: true },
-      }); 
+class MotorcycleODM extends AbstractODM<IMotorcycle> {
+  constructor() {
+    const schema = new Schema<IMotorcycle>({
+      model: { type: String, required: true },
+      year: { type: Number, required: true },
+      color: { type: String, required: true },
+      status: { type: Boolean, required: false },
+      buyValue: { type: Number, required: true },
+      category: { type: Number, required: true },
+      engineCapacity: { type: Number, required: true },
+    }); 
           
-      this.model = models.cars || model('cars', this.schema);
-    }
-  
-    public async create(motorcycle: IMotorcycle): Promise<IMotorcycle> {
-      return this.model.create({ ...motorcycle });
-    }
+    super(schema, 'motocycles');
   }
-  
-  export default MotorcycleODM;
+}
+
+export default MotorcycleODM;
