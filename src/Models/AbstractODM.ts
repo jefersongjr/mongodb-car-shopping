6c0ab2f8478df6c9cd5cac56e83aa63825eb1490
jefersongjr <jeferson_gjr@hotmail.com>
2022-11-21 18:52:29 -0300

    🚨 corrigindo o lint

--- src/Models/AbstractODM.ts
+++ src/Models/AbstractODM.ts
@@ -1,42 +1,41 @@
 import {
-    isValidObjectId,
-    Model,
-    model,
-    models,
-    Schema,
-    UpdateQuery,
-  } from 'mongoose';
+  Model,
+  model,
+  models,
+  Schema,
+  UpdateQuery,
+} from 'mongoose';
   
-  abstract class AbstractODM<T> {
-    private schema: Schema;
-    private model: Model<T>;
-    protected modelName: string;
+abstract class AbstractODM<T> {
+  private schema: Schema;
+  private model: Model<T>;
+  protected modelName: string;
   
-    constructor(schema: Schema, modelName: string) {
-      this.schema = schema;
-      this.modelName = modelName;
-      this.model = models[this.modelName] || model(this.modelName, this.schema);
-    }
-  
-    public async create(obj: T): Promise<T> {
-      return this.model.create({ ...obj });
-    }
-  
-    public async find(): Promise<T[]> {
-      return this.model.find();
-    }
-  
-    public async findById(id: string): Promise<T[]> {
-      return this.model.find({ id });
-    }
-  
-    public async updateCar(_id: string, obj: T): Promise<T | null> {  
-      return this.model.findByIdAndUpdate(
-        { _id },
-        { ...obj } as UpdateQuery<T>,
-        { new: true },
-      );
-    }
+  constructor(schema: Schema, modelName: string) {
+    this.schema = schema;
+    this.modelName = modelName;
+    this.model = models[this.modelName] || model(this.modelName, this.schema);
   }
   
-  export default AbstractODM;
+  public async create(obj: T): Promise<T> {
+    return this.model.create({ ...obj });
+  }
+  
+  public async find(): Promise<T[]> {
+    return this.model.find();
+  }
+  
+  public async findById(id: string): Promise<T[]> {
+    return this.model.find({ id });
+  }
+  
+  public async updateCar(_id: string, obj: T): Promise<T | null> {  
+    return this.model.findByIdAndUpdate(
+      { _id },
+      { ...obj } as UpdateQuery<T>,
+      { new: true },
+    );
+  }
+}
+  
+export default AbstractODM;
