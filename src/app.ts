82fa87a802f938f5776b7e9f19654362783429fa
jefersongjr <jeferson_gjr@hotmail.com>
2022-11-21 22:50:57 -0300

    ✨ feat: chamando a rota Motorcycles

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import ErrorHandler from './Middlewares/ErrorHandler';
 import carRoute from './Routes/CarRouter';
+import MotorcycleRoute from './Routes/MotorcycleRouter';
 
 const app = express();
 
 app.use(express.json());
 app.use(carRoute);
+app.use(MotorcycleRoute);
 app.use(ErrorHandler.handle);
 
 export default app;
