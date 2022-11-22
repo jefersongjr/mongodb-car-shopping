2f992d01566337ea272e58a34ff60fb4ec21287f
jefersongjr <jeferson_gjr@hotmail.com>
2022-11-21 22:51:33 -0300

    ✨ feat: criando controller da rota Motorcycle

---
+++ src/Controllers/MotorcycleController.ts
@@ -0,0 +1,42 @@
+import { Request, Response, NextFunction } from 'express';
+import IMotorcycle from '../Interfaces/IMotorcycle';
+import MotorcycleService from '../Services/MotorcycleService';
+
+class MotorcycleController {
+  private req : Request;
+  private res : Response;
+  private next: NextFunction;
+  private service: MotorcycleService;
+
+  constructor(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) {
+    this.req = req;
+    this.res = res;
+    this.next = next;
+    this.service = new MotorcycleService();
+  }
+
+  public async createNewMotorcycle() {
+    const motorcycle: IMotorcycle = {
+      model: this.req.body.model,
+      year: this.req.body.year,
+      color: this.req.body.color,
+      status: this.req.body.status,
+      buyValue: this.req.body.buyValue,
+      category: this.req.body.category,
+      engineCapacity: this.req.body.engineCapacity,
+    };
+        
+    try {
+      const newMotorcycle = await this.service.addMotorcycle(motorcycle);
+      return this.res.status(201).json(newMotorcycle);
+    } catch (error) {
+      this.next(error);
+    }
+  }
+} 
+
+export default MotorcycleController;
