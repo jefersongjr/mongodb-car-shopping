a1118f0cbeceeef3c5c72d5ba59e4023e0827204
jefersongjr <jeferson_gjr@hotmail.com>
2022-11-22 09:14:34 -0300

    ✨ feat: criando service para listar  motorcycle

--- src/Services/MotorcycleService.ts
+++ src/Services/MotorcycleService.ts
@@ -2,9 +2,11 @@ import Motorcycle from '../Domains/Motorcycle';
 import IMotorcycle from '../Interfaces/IMotorcycle';
 import MotorcycleODM from '../Models/MotorcycleODM';
 import CategoryFactory from '../utils/categoryFactory';
+import ThrowException from '../Middlewares/exceptions/ThrowException';
+import { isValidObjectId } from 'mongoose';
 
 class MotorcycleService {
-  private createMotorcycleDomain(motorcycle: IMotorcycle | null): Motorcycle | undefined | null {
+  private createMotorcycleDomain(motorcycle: IMotorcycle | null): Motorcycle | null {
     if (motorcycle) {
       return new Motorcycle(motorcycle);
     }
@@ -20,6 +22,27 @@ class MotorcycleService {
     );
 
     return this.createMotorcycleDomain(newMoto);
+  }
+
+  public async getMotorcycles() {
+    const allMotorcycles = await this.motorcycleODM.find();
+    if (!allMotorcycles) throw new ThrowException(422, 'Schema vázio');
+    console.log(allMotorcycles)
+    const motorcyclesArray = allMotorcycles.map((motorcycle) => this.createMotorcycleDomain(motorcycle));
+
+    return motorcyclesArray;
+  }
+  
+  public async getOnemotorcyclesArrayById(id: string) {
+    if (!isValidObjectId(id)) throw new ThrowException(422, 'Invalid mongo id');
+    const motorcycles = await this.motorcycleODM.findById(id);
+    
+    const motorcyclesArray = motorcycles.find((motorcycle) => motorcycle.id === id);
+    console.log(motorcyclesArray)
+  
+    if (!motorcyclesArray) throw new ThrowException(404, 'Motorcycle not found');
+
+    return this.createMotorcycleDomain(motorcyclesArray);
   }
 }
 
