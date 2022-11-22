900abde8de507a24fb3da2df5826e5341c417afa
jefersongjr <jeferson_gjr@hotmail.com>
2022-11-22 09:13:01 -0300

    ✨ feat: criando rota 'get' para motorcycles

--- src/Routes/MotorcycleRouter.ts
+++ src/Routes/MotorcycleRouter.ts
@@ -8,4 +8,15 @@ MotorcycleRoute.post(
   (req, res, next) => new MotorcycleController(req, res, next).createNewMotorcycle(),
 );
 
+MotorcycleRoute.get(
+  '/motorcycles',
+  (req, res, next) => new MotorcycleController(req, res, next).getAllMotorcycle(),
+);
+
+MotorcycleRoute.get(
+  '/motorcycles/:id',
+  (req, res, next) => new MotorcycleController(req, res, next).getById(),
+);
+
+
 export default MotorcycleRoute; 
